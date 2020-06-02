84d643c9733fb7b2b1a6339ff0a33eff165194bf
Cristian Garcia <eminemko@hotmail.com>
2020-06-02 15:27:50 -0500

    Picked color Add a click event listener to each squre, to know what square was clicked and get it's rgb color to compare the winner color.

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ var colors = [
 ];
 
 var squares =  document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
 
 squares.forEach(function(square, num){
 square.style.backgroundColor = colors[num];
+square.addEventListener("click",function(){
+    if(this.style.backgroundColor == pickedColor){
+        alert("You win");
+    }
+});
 });
